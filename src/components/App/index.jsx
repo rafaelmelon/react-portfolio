f773eb94e88b8d730ee9cf3000d73af7c4beae58
Rafael Melón <info@rafaelmelon.es>
2017-09-05 14:14:27 +0200

    Init config react

---
+++ src/components/App/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+
+class App extends Component {
+  render () {
+    return (
+      <div>
+        HOLA APP
+      </div>
+    )
+  }
+}
+
+export default App
