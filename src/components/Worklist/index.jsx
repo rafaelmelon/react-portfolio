c820a8a0401b998e67edf3e29454c7e21a351606
Rafael Melón <info@rafaelmelon.es>
2017-09-06 20:52:53 +0200

    GET xml list Medium post

--- src/components/Worklist/index.jsx
+++ src/components/Worklist/index.jsx
@@ -1,13 +1,51 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
-const Worklist = () => {
-  return (
-    <section>
+class Worklist extends Component {
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      works: []
+    }
+  }
+
+  componentWillMount () {
+    const URL = 'https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40susana.vazquez.rguez'
+    fetch(URL).then(res => res.json())
+    .then(data => {
+      console.log(data.items)
+
+      let worksArray = []
+      data.items.forEach(item => {
+        let workObject = {
+          id: item.guid.split("/").pop(),
+          title: item.title,
+          image: item.description.split('<img alt="" src="').pop().split('">').shift(),
+          type: item.categories[0],
+        }
+        worksArray.push(workObject)
+      })
+      this.setState({ works: worksArray })
+    })
+    .catch((error) => console.log(error))
+  }
+
+  render () {
+    return (
       <div>
-        WORKSSSSSS
+        { this.state.works.map(item =>
+          <article key={item.id}>
+            <div>
+              <small>{ item.type }</small>
+              <h3>{ item.title }</h3>
+            </div>
+            <img src={ item.image } />
+          </article>
+        ) }
       </div>
-    </section>
-  )
+    )
+  }
 }
 
 export default Worklist
